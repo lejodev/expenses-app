5d0572999a3f99e30490211f1ad14b39adfd0140
lejodev <alejandro.rv97@gmail.com>
2022-04-26 13:53:37 -0500

    Docker feature added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here will be placed the expenses app
         </p>
         <a
           className="App-link"

24fdbf9c4e7f6298afeaef40e6fb19a3625d4f03
lejodev <alejandro.rv97@gmail.com>
2022-05-07 22:30:55 -0500

    Component import in test fixed

--- src/components/__tests__/expenses/Expenses.test.js
+++ src/components/__tests__/expenses/Expenses.test.js
@@ -1,13 +1,13 @@
 import { render, screen, cleanup } from "@testing-library/react";
 // import "@testing-library/jest-dom";
-import Main from "../../main/Main"; 
+import Expenses from "../../expenses/Expenses"; 
 import { MemoryRouter as Router } from "react-router-dom";
 
 describe("Expenses main page", () => {
   it("Displays the expenses main page", () => {
     render(
       <Router>
-        <Main />
+        <Expenses />
       </Router>
     );
     expect(screen.queryByText(/expenses/i)).toBeInTheDocument();
